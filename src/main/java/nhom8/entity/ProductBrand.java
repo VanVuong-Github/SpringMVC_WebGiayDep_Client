1a78bcb515ee54d3e6c360a22904a9a8bcecca7e
Andy Lai <lai.vanvuong2866@gmail.com>
2022-04-24 18:31:52 +0700

    input entities

---
+++ src/main/java/nhom8/entity/ProductBrand.java
@@ -0,0 +1,17 @@
+package nhom8.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+public class ProductBrand {
+	private String id;
+    private String name;
+}
---
+++ src/main/java/nhom8/entity/ProductType.java
@@ -0,0 +1,17 @@
+package nhom8.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+public class ProductType {
+	private String id;
+    private String name;
+}
