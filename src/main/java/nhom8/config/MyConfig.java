59c956cf385c518ae7e41f1fa4c93f95f3bc199d
Andy Lai <lai.vanvuong2866@gmail.com>
2022-04-24 18:07:21 +0700

    initial commit

---
+++ src/main/java/nhom8/config/MyConfig.java
@@ -0,0 +1,30 @@
+package nhom8.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"nhom8"})
+@PropertySource("classpath:application.properties")
+public class MyConfig {
+	
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
